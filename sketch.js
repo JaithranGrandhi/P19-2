104a33e5455c2c9d7e0292a88d71334e088d5da9
JaithranGrandhi <93184979+JaithranGrandhi@users.noreply.github.com>
2021-12-17 14:08:26 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obsta
 
 var score;
 var gameOverImg,restartImg
-var jumpSound , checkPointSound, dieSound
+var jumpSound , dieSound
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
@@ -32,7 +32,7 @@ function preload(){
   
   jumpSound = loadSound("jump.mp3")
   dieSound = loadSound("die.mp3")
-  checkPointSound = loadSound("checkPoint.mp3")
+//   checkPointSound = loadSound("checkPoint.mp3")
 }
 
 function setup() {
